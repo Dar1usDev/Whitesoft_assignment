57a5cc0ba1c393da35781b40f3e77ac60d6ae6fb
Dmitry <Selesnjow@gmail.com>
2022-03-03 19:25:30 +1000

    Добавьте файлы проекта.

---
+++ Whitesoft_assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Text.Json;
+using Whitesoft_assignment;
+
+string dataFileURL = "https://raw.githubusercontent.com/thewhitesoft/student-2022-assignment/main/data.json";
+Stream dataStream = WebRequest.Create(dataFileURL).GetResponse().GetResponseStream();
+StreamReader dataReader = new StreamReader(dataStream);
+
+string replacementFileURL = "https://raw.githubusercontent.com/thewhitesoft/student-2022-assignment/main/replacement.json";
+Stream replacementStream = WebRequest.Create(replacementFileURL).GetResponse().GetResponseStream();
+StreamReader replacementReader = new StreamReader(replacementStream);
+
+List<KeyValuePair<string, string>>? replacementPairs = JsonSerializer.Deserialize<List<KeyValuePair<string, string>>?>(replacementStream);
+
+replacementPairs.ForEach(p => Console.WriteLine($"{p.Key} + {p.Value}"));
+
+
+
+
---
+++ Whitesoft_assignment/ReplacementPair.cs
@@ -0,0 +1,23 @@
+﻿namespace Whitesoft_assignment
+{
+    /// <summary>
+    /// Structure for replacement.json
+    /// </summary>
+    internal class ReplacementPair
+    {
+        private string Key
+        {
+            get;
+        }
+        private string Value
+        {
+            get;
+        }
+
+        ReplacementPair(string key, string value)
+        {
+            Key = key; 
+            Value = value;
+        }
+    }
+}
